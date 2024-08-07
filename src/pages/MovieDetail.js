4b9eae75a60a5ee22dd6ef195ab8759902f643d0
Uts13 <utsav.chaturvedi16@gmail.com>
2024-08-07 13:19:47 +0530

    Update IMDB Link

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -72,8 +72,8 @@ export const MovieDetail = () => {
           </p>
 
           <p className="my-4">
-            <span className="mr-2 font-bold">IMDB Code:</span>
-            <a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noreferrer">{movie.imdb_id}</a>
+            <span className="mr-2 font-bold">IMDB Link:</span>
+            <a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noreferrer">Click here</a>
           </p>
 
         </div>
